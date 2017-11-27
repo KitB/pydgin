0420af50672b1615c037e2d6edd8e5d06a588005
Kit Barnes <kit@ninjalith.com>
2017-11-27 11:45:03 +0000

    Make many_model take advantage of being embarassingly parallel

--- many_model.py
+++ many_model.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import multiprocessing
 import os
 
 import progress.bar
@@ -8,22 +9,33 @@ import creole_model_numpy as model
 
 
 class ModelRunner(object):
-    def __init__(self, learning_rate_coordinated, learning_rate_uncoordinated):
+    def __init__(self, learning_rate_coordinated, learning_rate_uncoordinated, epochs):
         self.lr_c = learning_rate_coordinated
         self.lr_u = learning_rate_uncoordinated
+        self.epochs_path = epochs
 
-    def __call__(self, populations_path, epochs_path):
-        m = model.Model(populations_path, epochs_path, self.lr_c, self.lr_u)
+    def __call__(self, element):
+        i, populations_path = element
+        m = model.Model(populations_path, self.epochs_path, self.lr_c, self.lr_u)
         m.run()
-        return m
+        return i, m
 
 
 def run_dir(dir_path, epochs_path, learning_rate_coordinated, learning_rate_uncoordinated):
-    run_model = ModelRunner(learning_rate_coordinated, learning_rate_uncoordinated)
+    pool = multiprocessing.Pool(processes=8)
+    run_model = ModelRunner(learning_rate_coordinated, learning_rate_uncoordinated, epochs_path)
     pops = sorted(os.listdir(dir_path))
+
+    # Progress bar stuff
     bar = progress.bar.IncrementalBar('Running models')
-    models = [run_model(os.path.join(dir_path, pop), epochs_path) for pop in bar.iter(pops)]
-    return models
+    bar.max = len(pops)
+
+    pops = [os.path.join(dir_path, pop) for pop in pops]
+
+    models = pool.imap_unordered(run_model, enumerate(pops))
+    out = [model for model in bar.iter(models)]  # Consume the iterator (meaning wait for all of the processes to complete)
+
+    return [m for i, m in sorted(out)]  # Sort by the first element (their index) and return the second
 
 
 def main():
